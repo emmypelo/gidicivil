593cbfd3af21a56c1c0e6b4d8b68be0448a69176
Emmypelo <Emmypeloguns@gmail.com>
2024-08-01 18:56:55 +0100

    further

--- components/data.js
+++ components/data.js
@@ -24,7 +24,7 @@ export const productData = [
     id: 1,
     imageurl:
       "https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Colorful sneakers",
+    name: "Lagos",
     price: "$19.99",
     description: "Some text about the product..",
   },
@@ -32,7 +32,7 @@ export const productData = [
     id: 2,
     imageurl:
       "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    name: "Sport sneakers",
+    name: "Abuja",
     price: "$21.99",
     description: "Some text about the product..",
   },
@@ -40,7 +40,7 @@ export const productData = [
     id: 3,
     imageurl:
       "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "iWatch",
+    name: "Ekiti",
     price: "$99.99",
     description: "Some text about the product..",
   },
@@ -48,7 +48,7 @@ export const productData = [
     id: 4,
     imageurl:
       "https://images.unsplash.com/photo-1610824352934-c10d87b700cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Water Bottle",
+    name: "London",
     price: "$14.99",
     description: "Some text about the product..",
   },
@@ -56,7 +56,7 @@ export const productData = [
     id: 5,
     imageurl:
       "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Vans sneakers",
+    name: "Kenya",
     price: "$38.99",
     description: "Some text about the product..",
   },
@@ -64,7 +64,7 @@ export const productData = [
     id: 6,
     imageurl:
       "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Coco Noir",
+    name: "Dubai",
     price: "$149.99",
     description: "Some text about the product..",
   },
@@ -72,7 +72,7 @@ export const productData = [
     id: 7,
     imageurl:
       "https://images.unsplash.com/photo-1589782182703-2aaa69037b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Sunglasses",
+    name: "Dakota",
     price: "$38.99",
     description: "Some text about the product..",
   },
@@ -80,7 +80,7 @@ export const productData = [
     id: 8,
     imageurl:
       "https://images.unsplash.com/photo-1625772452859-1c03d5bf1137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    name: "Dove cream",
+    name: "Honolulu",
     price: "$49.99",
     description: "Some text about the product..",
   },
